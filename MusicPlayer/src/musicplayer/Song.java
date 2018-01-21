8dd694c3fb2b0db0c85b9a587d3dcbc2717764c3
p312734 <oliver.balb@gmx.de>
2018-01-21 22:31:05 +0100

    5.7.1 Statische Felder und Methoden

--- MusicPlayer/src/musicplayer/Song.java
+++ MusicPlayer/src/musicplayer/Song.java
@@ -6,13 +6,25 @@ import java.time.Duration;
 public class Song {
  
 	/* Just a class */
-	 
+	
+	private static long gesamtLaenge; 
 	 
 	private String titel;
 	private String interpret;
 	private Duration laenge;
 	
+
+	public static long getGesamtLaenge() {
+		return Song.gesamtLaenge;
+	}
 	
+	
+	public void setLaengeInSekunden(int laengeInSekunden) {
+		Song.gesamtLaenge -= this.getLaengeInSekunden();
+		Song.gesamtLaenge += laengeInSekunden;
+		this.setLaengeInSekunden(laengeInSekunden);
+	}
+
 	public Song (String interpret, String titel, long laengeInSekunden) {
 		/**
 		 * @param interpret String
